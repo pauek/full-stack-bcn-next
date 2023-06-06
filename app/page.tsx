f28048b7fa17a5a776636706631aa454acd5dab8
Pau Fernandez <pau.fernandez@upc.edu>
2023-06-06 17:08:23 +0200

    Bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ type Props = {
 const Session = ({ item, partSlug }: Props & { partSlug: string }) => {
   const { slug } = item.metadata;
   return (
-    <Link href={`/${partSlug}/${slug}`} >
+    <Link href={`/${partSlug}/${slug}`}>
       <div className="text-sm hover:bg-stone-100">{item.name}</div>
     </Link>
   );
@@ -21,7 +21,10 @@ const Part = ({ item }: Props) => {
     <div className="border p-2 pt-1 mb-2">
       <h4 className="text-stone-500 font-bold mb-2">{name}</h4>
       <div className="pl-3">
-        {children && children.map((child) => <Session partSlug={slug} item={child} />)}
+        {children &&
+          children.map((child) => (
+            <Session key={child.path} partSlug={slug} item={child} />
+          ))}
       </div>
     </div>
   );
