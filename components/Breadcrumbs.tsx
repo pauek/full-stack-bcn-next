eb6bea3d439c54f61ba594c764149e53a2ed961e
Pau Fernández <pau.fernandez@upc.edu>
2023-07-28 15:56:17 +0200

    Quito Crumb

--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -12,25 +12,14 @@ type CrumbProps = {
 const CrumbLink = ({ crumb, position, isLast }: CrumbProps) => {
   if (isLast) {
     return <div className="select-none text-stone-400">{crumb.name}</div>;
-  } else  {
+  } else {
     return <Link href={`/content/${crumb.path.join("/")}`}>{crumb.name}</Link>;
   }
 };
 
-const Crumb = ({ crumb, position, isLast }: CrumbProps) => {
-  return (
-    <>
-      <div className={"mx-2 text-stone-300 "}>
-        <BreadCrumbsSlash />
-      </div>
-      <CrumbLink crumb={crumb} position={position} isLast={isLast} />
-    </>
-  );
-};
-
 type Props = {
   path: string[];
-}
+};
 export default async function Breadcrumbs({ path }: Props) {
   const [__part, ...crumbs] = await getBreadcrumbs(path);
   return (
@@ -38,12 +27,16 @@ export default async function Breadcrumbs({ path }: Props) {
       <div className="hidden sm:flex flex-row items-center">
         {crumbs.length > 1 &&
           crumbs.map((cr, i) => (
-            <Crumb
-              key={cr.path.join("/")}
-              crumb={cr}
-              position={i}
-              isLast={i === crumbs.length - 1}
-            />
+            <>
+              <div className={"mx-2 text-stone-300 "}>
+                <BreadCrumbsSlash />
+              </div>
+              <CrumbLink
+                crumb={cr}
+                position={i}
+                isLast={i === crumbs.length - 1}
+              />
+            </>
           ))}
       </div>
       {crumbs.length > 0 && (
