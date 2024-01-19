9bca53e423377dfa37cfd4b305db8dead56adba1
Pau Fernández <pau.fernandez@upc.edu>
2024-01-19 20:05:42 +0100

    Migración + walk-metadata

--- data/schema.ts
+++ data/schema.ts
@@ -1,32 +1,28 @@
 import { base64ToBytes, bytesToBase64 } from "@/lib/utils";
-import {
-  customType,
-  integer,
-  json,
-  pgTable,
-  primaryKey,
-  text,
-} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { customType, integer, json, pgTable, text } from "drizzle-orm/pg-core";
 
 export const pieces = pgTable("pieces", {
   id: text("piece_id").primaryKey(),
   name: text("name").notNull(),
-  path: text("string"),
-  diskpath: text("string"),
+  path: text("string").notNull(),
+  diskpath: text("string").notNull(),
+  numSlides: integer("num_slides").notNull(),
+  hasDoc: integer("has_doc").notNull(),
+
+  parent: text("parent_id"),
   index: integer("index"),
   metadata: json("metadata"),
 });
-
-export const children = pgTable(
-  "children",
-  {
-    parent: text("parent_piece_id").notNull(),
-    child: text("child_piece_id").notNull(),
-  },
-  (table) => ({
-    pk: primaryKey({ columns: [table.parent, table.child] }),
-  })
-);
+export const piecesRelations = relations(pieces, ({ one, many }) => ({
+  parent: one(pieces, {
+    fields: [pieces.parent],
+    references: [pieces.id],
+    relationName: "parent_child",
+  }),
+  children: many(pieces, { relationName: "parent_child" }),
+  files: many(files, { relationName: "piece_files" }),
+}));
 
 const base64bytes = customType<{
   data: Uint8Array;
@@ -45,7 +41,14 @@ const base64bytes = customType<{
 
 export const files = pgTable("files", {
   id: text("file_id").primaryKey(),
-  piece: text("piece_id").references(() => pieces.id),
+  piece: text("piece_id").notNull(),
   name: text("name").notNull(),
   data: base64bytes("data"),
 });
+export const filesRelations = relations(files, ({ one }) => ({
+  piece: one(pieces, {
+    fields: [files.piece],
+    references: [pieces.id],
+    relationName: "piece_files",
+  }),
+}));
--- scripts/walk-metadata.ts
+++ scripts/walk-metadata.ts
@@ -1,17 +1,15 @@
-import { getPiece, readMetadata, writeMetadata } from "@/lib/files/files";
+import { getPiece, updateMetadata } from "@/lib/files/files";
 import { walkContentPieces } from "@/lib/files/hashes";
 
 const fullstack = await getPiece(["fullstack"]);
 if (!fullstack) {
   throw `Course "fullstack" not found!`;
 }
-
-walkContentPieces(fullstack, async (piece, _) => {
-  const metadata = await readMetadata(piece.diskpath);
-  for (const field in metadata) {
-    if (Array.isArray(metadata[field])) {
-      console.log(piece.diskpath, field, metadata[field]);
+await walkContentPieces(fullstack, async (piece, _) => {
+  await updateMetadata(piece.diskpath, (metadata) => {
+    if ("slideHashes" in metadata) {
+      console.log(piece.idpath.join("/"));
+      delete metadata.slideHashes;
     }
-  }
-  await writeMetadata(piece.diskpath, metadata);
-})
+  });
+});
