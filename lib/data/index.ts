3e886f61a14ca09979e5437ffa47713df96720cd
Pau Fernández <pau.fernandez@upc.edu>
2024-01-21 18:57:49 +0100

    Escojo backend en función del entorno

--- lib/data/index.ts
+++ lib/data/index.ts
@@ -3,4 +3,14 @@ import { DataBackend } from "./data-backend";
 import { backend as filesBackend } from "@/lib/data/files";
 import { backend as dbBackend } from "@/lib/data/db";
 
-export default process.env.NODE_ENV === "production" ? dbBackend : (filesBackend as DataBackend);
+const initBackend = (): DataBackend => {
+  if (process.env.MODE! === "local") {
+    console.log("--> Using files backend.");
+    return filesBackend;
+  } else {
+    console.log("--> Using database backend.");
+    return dbBackend;
+  }
+}
+
+export default initBackend();
