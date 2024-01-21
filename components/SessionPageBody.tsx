f0f7c62aeedae57b7a8f3203c8e8643e28e4c9d2
Pau Fernández <pau.fernandez@upc.edu>
2024-01-21 19:59:35 +0100

    Mejoro estilo de una sesión

--- components/SessionPageBody.tsx
+++ components/SessionPageBody.tsx
@@ -2,36 +2,24 @@ import data from "@/lib/data";
 import ChapterItem from "./ChapterItem";
 import StaticLayout from "./StaticLayout";
 
-export default async function SessionPageBody({
-  idpath,
-}: {
-  idpath: string[];
-}) {
+export default async function SessionPageBody({ idpath }: { idpath: string[] }) {
   const session = await data.getPieceWithChildren(idpath);
   if (session === null) {
     throw `Session with path ${idpath} not found`;
   }
   return (
     <StaticLayout path={idpath}>
-      <div className="max-w-[54em] w-full m-auto pb-6">
-        <div className="mx-4">
-          <div id="top" className="absolute top-0" />
-          <div className="pt-8 border-b mb-6">
-            <div className="text-stone-400 mb-0 text-xs">
-              SESSION {session.metadata.index}
-            </div>
-            <h2 className="p-0 pb-2">{session.name}</h2>
+      <div className="md:w-[54em] md:m-auto md:mt-4 m-4 pb-5 border rounded-lg bg-white">
+          <div className="pt-4 mx-4">
+            <div className="text-stone-400 mb-0 text-xs">SESSION {session.metadata.index}</div>
+            <h2 className="p-0 pb-3">{session.name}</h2>
           </div>
-          <div className="flex flex-col gap-4">
+          <div className="border-b mb-5"></div>
+          <div className="flex flex-col gap-4 mx-5">
             {session.children?.map((piece, index) => (
-              <ChapterItem
-                key={piece.id}
-                index={index + 1}
-                chapter={piece}
-              />
+              <ChapterItem key={piece.id} index={index + 1} chapter={piece} />
             ))}
           </div>
-        </div>
       </div>
     </StaticLayout>
   );
