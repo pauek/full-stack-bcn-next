994560dd586bf62cddd23640b353722704ac735d
Pau Fernández <pau.fernandez@upc.edu>
2024-01-30 13:44:48 +0100

    Bug fix

--- components/DarkModeAwareBody.tsx
+++ components/DarkModeAwareBody.tsx
@@ -4,21 +4,34 @@ import { cn } from "@/lib/utils";
 import { ComponentProps, PropsWithChildren, useState } from "react";
 import { DarkModeContext } from "./DarkMode";
 
+const loadDarkModeState = () => {
+  if (typeof window !== "undefined") {
+    return window.localStorage.getItem("darkMode") === "yes";
+  }
+};
+
+const saveDarkModeState = (value: boolean) => {
+  if (typeof window !== "undefined") {
+    return window.localStorage.setItem("darkMode", value ? "yes" : "no");
+  }
+};
+
 export default function DarkModeAwareBody({
   className,
   children,
 }: PropsWithChildren<ComponentProps<"body">>) {
-  const darkModeSetting = localStorage.getItem("darkMode") === "yes";
-  const [dark, setDark] = useState(darkModeSetting);
+  const [dark, setDark] = useState(loadDarkModeState() || false);
 
   const setDarkMode = (dark: boolean) => {
-    localStorage.setItem("darkMode", dark ? "yes" : "no");
+    saveDarkModeState(dark);
     setDark(dark);
   };
 
   return (
     <html className={cn(className, dark ? "dark" : "")}>
-      <DarkModeContext.Provider value={{ dark, setDark: setDarkMode }}>{children}</DarkModeContext.Provider>
+      <DarkModeContext.Provider value={{ dark, setDark: setDarkMode }}>
+        {children}
+      </DarkModeContext.Provider>
     </html>
   );
 }
