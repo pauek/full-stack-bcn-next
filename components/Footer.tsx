f2f30283fe0f5f4fbbf9644e855eb35bc78bd1c6
Pau Fernández <pau.fernandez@upc.edu>
2024-01-30 12:25:11 +0100

    Improve Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default async function Footer() {
   return (
-    <footer className="flex flex-col items-center pt-6 pb-4 text-xs text-stone-400">
+    <footer className="w-full flex flex-col items-center mt-6 pt-6 pb-4 text-xs text-stone-400 bg-stone-100 border-t shadow-inner">
       <div>&copy; Pau Fernández, {new Date().getFullYear()}</div>
     </footer>
   );
