b13bb085783183a625493378798888987a0eaf78
Pau Fernández <pau.fernandez@upc.edu>
2024-01-30 14:22:11 +0100

    Migrate que funciona

--- data/migrate.ts
+++ data/migrate.ts
@@ -1,4 +1,12 @@
-import { db } from "@/lib/data/db/db";
+import * as schema from "@/data/schema";
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
+import ws from 'ws';
+
+neonConfig.webSocketConstructor = ws;
+const sql = new Pool({ connectionString: process.env.DATABASE_URL! });
+
+export const db = drizzle(sql, { schema });
 
 await migrate(db, { migrationsFolder: "drizzle" });
