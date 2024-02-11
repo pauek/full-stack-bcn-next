52682fec2fe7e82f802afee143f1ff015601c3dc
Pau Fernández <pau.fernandez@upc.edu>
2024-02-11 13:07:40 +0100

    Simplify Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,30 +1,12 @@
 "use client";
 
+import { ContentPiece } from "@/lib/adt";
 import Link from "next/link";
 import { useSelectedLayoutSegments } from "next/navigation";
 import DarkModeSwitch from "./DarkModeSwitch";
-import { HeaderNavigationMenu } from "./HeaderNavigationMenu";
-import MobileMenu from "./MobileMenu";
-import BreadCrumbsSlash from "./icons/BreadCrumbsSlash";
-import { useEffect, useState } from "react";
-import { ContentPiece } from "@/lib/adt";
-import type { CrumbData } from "@/lib/data/data-backend";
-import { backend as data } from "@/lib/data/db";
-import { getBreadcrumbData } from "@/lib/data/common";
 
 export default function Header({ course }: { course: ContentPiece }) {
   const [_c, _courseId, part, session, chapter] = useSelectedLayoutSegments();
-  const [partCrumb, setPart] = useState<CrumbData | undefined>();
-  const [crumbs, setCrumbs] = useState<CrumbData[]>([]);
-
-  useEffect(() => {
-    const idpath = [course.id, part, session, chapter].filter(Boolean);
-    console.log("Idpath", { idpath });
-    getBreadcrumbData.call(data, ...idpath).then(([part, ...rest]) => {
-      setPart(part);
-      setCrumbs(rest);
-    });
-  }, [course.id, part, session, chapter]);
 
   return (
     <header
@@ -33,14 +15,12 @@ export default function Header({ course }: { course: ContentPiece }) {
         "flex-row items-center border-b z-20 shadow-sm overflow-visible"
       }
     >
-      <div className="md:hidden flex flex-col justify-center items-center mr-2">
-        <MobileMenu crumbs={crumbs} />
-      </div>
-      <Link href="/" className="font-bold ml-3 whitespace-nowrap overflow-ellipsis overflow-clip header">
+      <Link href="/" className="font-bold ml-5">
         {course.name}
       </Link>
+
       <div className="flex-1 block"></div>
-      
+
       <DarkModeSwitch className="mr-5" />
     </header>
   );
