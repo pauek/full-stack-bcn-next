37205ab1c53e2c15f22702790e5e8315936c2231
Pau Fernández <pau.fernandez@upc.edu>
2024-02-12 18:01:55 +0100

    Fixed logging of uploaded files

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,4 @@
+import { FileTypeEnum } from "@/data/schema";
 import chalk from "chalk";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
@@ -38,8 +39,8 @@ export const showExecutionTime = async <T>(func: () => Promise<T>, msg?: string)
   const start = Date.now();
   await func();
   const end = Date.now();
-  console.log(chalk.yellow(`\n[${msg ? `${msg}: ` : ``}${(end - start)/1000}s]`));
-}
+  console.log(chalk.yellow(`\n[${msg ? `${msg}: ` : ``}${(end - start) / 1000}s]`));
+};
 
 export const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -49,9 +50,32 @@ export const chunkArray = (array: Array<any>, chunkSize: number) => {
     result.push(array.slice(i, i + chunkSize));
   }
   return result;
-}
+};
 
 // https://stackoverflow.com/questions/64928212/how-to-use-promise-allsettled-with-typescript
-export const isFulfilled = function<T>(input: PromiseSettledResult<T>): input is PromiseFulfilledResult<T> {
-  return input.status === 'fulfilled';
-}
+export const isFulfilled = function <T>(
+  input: PromiseSettledResult<T>
+): input is PromiseFulfilledResult<T> {
+  return input.status === "fulfilled";
+};
+
+type LogFileOptions = { preserve?: boolean, color?: boolean }
+
+const logFile =
+  (options?: LogFileOptions) =>
+  (hash: string, filetype: FileTypeEnum, filename: string) => {
+    const { preserve = false, color = false } = options || {};
+
+    const _hash = color ? chalk.gray(hash) : hash;
+    const _filetype = color ? chalk.green(filetype) : filetype;
+    const _filename = color ? chalk.yellow(filename) : filename;
+
+    const line = `  ${_hash} ${_filetype} ${_filename}`;
+    const space = " ".repeat(process.stdout.columns - 1 - line.length);
+    
+    process.stdout.write(`${line}${space}${preserve ? "\n" : "\r"}`);
+  };
+
+export const logUploadedFile = logFile({ preserve: true, color: true });
+export const logPresentFile = logFile();
+
