984b852850a7b86fa10c4fddf838233fa447bd13
Pau Fernández <pau.fernandez@upc.edu>
2024-02-12 18:02:02 +0100

    Added border prop to Image

--- components/mdx/ImageForChapter.tsx
+++ components/mdx/ImageForChapter.tsx
@@ -1,10 +1,21 @@
 import Zoomable from "@/components/Zoomable";
 import { FileReference } from "@/lib/data/data-backend";
 import { attachmentUrl } from "@/lib/urls";
+import { cn } from "@/lib/utils";
 import NextImage from "next/image";
 
+type _ImageProps = {
+  border?: boolean;
+};
+
 export default function ImageForChapter(imageMap: Map<string, FileReference>) {
-  return function Image({ src, alt, width, height }: React.ComponentProps<"img">) {
+  return function Image({
+    src,
+    alt,
+    width,
+    height,
+    border = true,
+  }: React.ComponentProps<"img"> & _ImageProps) {
     // Hay que insertar el id del Chapter para que el documento
     // pueda referirse a la imagen con un path relativo
     if (!src) {
@@ -15,7 +26,7 @@ export default function ImageForChapter(imageMap: Map<string, FileReference>) {
       throw new Error(`Image "${src}" not found in imageMap!`);
     }
     return (
-      <div className="border my-4">
+      <div className={cn(border ? "border" : "", "my-4 flex justify-center items-center")}>
         <Zoomable>
           <NextImage
             src={attachmentUrl(fileref)}
