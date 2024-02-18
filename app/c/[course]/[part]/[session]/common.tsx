433883a7b28015e8eeeb843da67f7f3ce5e6a441
Pau Fernández <pau.fernandez@upc.edu>
2024-02-18 21:25:48 +0100

    Introduzco dos unstable_cache
    Que sí que se notan

--- app/c/[course]/[part]/[session]/common.tsx
+++ app/c/[course]/[part]/[session]/common.tsx
@@ -2,6 +2,7 @@ import { FileType } from "@/data/schema";
 import { ContentPiece } from "@/lib/adt";
 import data from "@/lib/data";
 import { isFulfilled } from "@/lib/utils";
+import { unstable_cache } from "next/cache";
 import { notFound } from "next/navigation";
 
 export type SessionPageProps = {
@@ -15,7 +16,10 @@ export type SessionPageProps = {
 export const getPieceWithChildrenOrNotFound = async ({ params }: SessionPageProps) => {
   const { course, part, session } = params;
   const idpath = [course, part, session];
-  const piece = await data.getPieceWithChildren(idpath);
+  const piece = await unstable_cache(
+    async () => await data.getPieceWithChildren(idpath),
+    ["piece-with-children", idpath.join("/")]
+  )();
   if (piece === null) {
     notFound();
   }
@@ -28,7 +32,10 @@ export const getAttachments = async (piece: ContentPiece, filetype: FileType) =>
   const result = await Promise.allSettled(
     children.map(async (chapter) => ({
       chapter,
-      attachments: await data.getPieceAttachmentList(chapter, filetype),
+      attachments: await unstable_cache(
+        async () => await data.getPieceAttachmentList(chapter, filetype),
+        [chapter.hash, filetype.toString()]
+      )(),
     }))
   );
 
