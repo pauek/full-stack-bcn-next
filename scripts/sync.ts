d6daf22d300b5fc9579f95a46d8063f20e64377d
Pau Fernández <pau.fernandez@upc.edu>
2024-05-06 14:44:14 +0200

    Bug fix

--- scripts/sync.ts
+++ scripts/sync.ts
@@ -1,12 +1,14 @@
-import { courseUpdateMetadata, filesBackend } from "@/lib/data/files";
+import { courseUpdateMetadata, filesBackend, okToSkipMissingHashes } from "@/lib/data/files";
 import { getRoot } from "@/lib/data/root";
 import { showExecutionTime } from "@/lib/utils";
 import { insertPieceWalker, uploadImages, walkFilesIfChanged, writeHashes } from "./lib";
 
 await showExecutionTime(async () => {
   const root = await getRoot(filesBackend);
+  await okToSkipMissingHashes(async () => {
+    await writeHashes();
+  });
   await courseUpdateMetadata(filesBackend, root);
-  await writeHashes();
   await walkFilesIfChanged(false, root.idpath, insertPieceWalker);
   await uploadImages();
 });
