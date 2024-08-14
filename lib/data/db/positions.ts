cb8e41f2d267435139220c9a719226389544fe71
Pau Fernández <pau.fernandez@upc.edu>
2024-08-14 15:51:48 +0200

    Refactors

--- lib/data/db/positions.ts
+++ lib/data/db/positions.ts
@@ -3,24 +3,17 @@ import { eq } from "drizzle-orm"
 import { db } from "./db"
 
 export const dbMapPositionsGetAll = async () => {
-  /*
-     Hay que devolver los items ordenados por nivel, para pintarlos en el orden correcto.
-  */
-  const results = await db.query.mapPositions.findMany({
+  //  Hay que devolver los items ordenados por nivel, para pintarlos en el orden correcto.
+  const positions = await db.query.mapPositions.findMany({
     with: {
       piece: {
         columns: { name: true },
         with: {
           children: {
-            columns: {
-              childHash: true,
-            },
+            columns: { childHash: true },
           },
           hashmapEntry: {
-            columns: {
-              idjpath: true,
-              level: true,
-            },
+            columns: { idjpath: true, level: true },
           },
         },
       },
@@ -28,14 +21,14 @@ export const dbMapPositionsGetAll = async () => {
   })
 
   // We sort the results here, instead of in the query (can't do it in Drizzle??)
-  results.sort((a, b) => {
-    const alev = a.piece.hashmapEntry?.level || -1
-    const blev = b.piece.hashmapEntry?.level || -1
-    return blev - alev
+  positions.sort((p1, p2) => {
+    const lev1 = p1.piece.hashmapEntry?.level || -1
+    const lev2 = p2.piece.hashmapEntry?.level || -1
+    return lev2 - lev1 // reverse sorted!
   })
 
   const maybeFind = (hash: string) => {
-    const index = results.findIndex((it) => it.pieceHash === hash)
+    const index = positions.findIndex((it) => it.pieceHash === hash)
     if (index === -1) {
       return null
     }
@@ -44,20 +37,24 @@ export const dbMapPositionsGetAll = async () => {
 
   // Once sorted, we want to relate the children to their parents by an index
   // so the children are just an index to the parent.
-  const cleanResults = results.map((result) => {
-    if (result.piece.hashmapEntry === null) {
-      throw new Error(`Piece ${result.piece.name} has no hashmap entry`)
+  const cleanResults = positions.map((pos) => {
+    if (pos.piece.hashmapEntry === null) {
+      throw new Error(`Piece ${pos.piece.name} has no hashmap entry`)
     }
+    const childrenIndices = pos.piece.children
+      .map((ch) => maybeFind(ch.childHash))
+      .filter((ch) => ch !== null)
+
     return {
-      left: result.left,
-      top: result.top,
-      width: result.width,
-      height: result.height,
-      name: result.piece.name,
-      pieceHash: result.pieceHash,
-      idjpath: result.piece.hashmapEntry.idjpath,
-      level: result.piece.hashmapEntry.level,
-      children: result.piece.children.map((ch) => maybeFind(ch.childHash)).filter(ch => ch !== null),
+      left: pos.left,
+      top: pos.top,
+      width: pos.width,
+      height: pos.height,
+      name: pos.piece.name,
+      pieceHash: pos.pieceHash,
+      idjpath: pos.piece.hashmapEntry.idjpath,
+      level: pos.piece.hashmapEntry.level,
+      children: childrenIndices,
     }
   })
 
