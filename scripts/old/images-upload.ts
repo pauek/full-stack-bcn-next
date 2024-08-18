573297d4a16c175f8695a20d0d5bac74bdac86fd
Pau Fernández <pau.fernandez@upc.edu>
2024-08-18 11:57:04 +0200

    Erased old code with errors (or fixed it)

--- scripts/old/images-upload.ts
+++ scripts/old/images-upload.ts
@@ -1,9 +1,9 @@
+import { filesGetRoot } from "@/lib/data/files/utils"
 import { showExecutionTime } from "@/lib/utils"
 import { uploadImages } from "../lib/lib"
-import { filesGetRootIdpath } from "@/lib/data/files/utils"
 
 await showExecutionTime(async () => {
-  await uploadImages(filesGetRootIdpath())
+  await uploadImages(await filesGetRoot())
 })
 
 process.exit(0) // Force exit to avoid waiting
