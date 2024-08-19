a548c6e8116cd19a140a61418e46534f3679ecc3
Pau Fernández <pau.fernandez@upc.edu>
2024-08-19 17:12:56 +0200

    Refactor of <Exercise />

--- components/Exercise.tsx
+++ components/Exercise.tsx
@@ -3,7 +3,7 @@ import { FileType } from "@/data/schema"
 import { ContentPiece } from "@/lib/adt"
 import data from "@/lib/data"
 import { FileReference } from "@/lib/data/data-backend"
-import { cn } from "@/lib/utils"
+import { cn, splitMarkdownPreamble } from "@/lib/utils"
 
 type ExerciseProps = {
   chapter: ContentPiece
@@ -15,22 +15,28 @@ export default async function Exercise({ index, chapter, exercise }: ExercisePro
   const images = await data.getPieceAttachmentList(chapter, FileType.image)
   const imageMap = new Map(images.map((ref) => [ref.filename, ref]))
 
+  if (content === null) {
+    return null
+  }
+  const { preamble, body } = splitMarkdownPreamble(content?.bytes.toString())
   return (
-    content && (
+    <>
+      <pre className="text-xs bg-red-100 border-none text-black">{JSON.stringify(content.metadata, null, 2)}</pre>
+
       <div className="md:flex md:flex-row flex-col mx-2.5 mb-2.5">
         <div
           className={cn(
             "mx-2.5 lg:ml-5 mt-[.7em] w-[1.8em] h-[1.8em]",
             "flex flex-col justify-center items-center",
-            "bg-primary text-background font-bold rounded-full",
+            "bg-primary text-background font-bold rounded-full"
           )}
         >
           {index}
         </div>
         <div className="flex-1">
-          <MdxDocument className="p-2.5" text={content.bytes.toString()} imageMap={imageMap} />
+          <MdxDocument className="p-2.5" text={body} imageMap={imageMap} />
         </div>
       </div>
-    )
+    </>
   )
 }
