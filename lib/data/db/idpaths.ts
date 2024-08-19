096c0bbac9c3e6be1ba5586f1c694c1995358da8
Pau Fernández <pau.fernandez@upc.edu>
2024-08-19 11:57:01 +0200

    Bug fix

--- lib/data/db/idpaths.ts
+++ lib/data/db/idpaths.ts
@@ -1,11 +1,13 @@
 import * as schema from "@/data/schema"
-import { like } from "drizzle-orm"
+import { eq, like } from "drizzle-orm"
 import { db } from "./db"
 
 export const getAllIdpaths = async (rootIdpath: string[]): Promise<string[][]> => {
   const result = await db.query.hashmap.findMany({
-    where: like(schema.hashmap.idpath, `${rootIdpath.join("/")}%`),
     columns: { idpath: true },
   })
-  return result.map(({ idpath }) => idpath)
+  const rootIdjpath = rootIdpath.join("/")
+  return result
+    .filter(({ idpath }) => idpath.join("/").startsWith(rootIdjpath))
+    .map(({ idpath }) => idpath)
 }
