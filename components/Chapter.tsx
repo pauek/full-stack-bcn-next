55abc69d269b68b902ef5bf62cc2416ff6235cbd
Pau Fernández <pau.fernandez@upc.edu>
2024-08-20 08:57:54 +0200

    splitMarkdownPreamble en doc.mdx

--- components/Chapter.tsx
+++ components/Chapter.tsx
@@ -1,6 +1,8 @@
 import { FileType } from "@/data/schema"
 import { ContentPiece } from "@/lib/adt"
 import data from "@/lib/data"
+import { FileBuffer } from "@/lib/data/data-backend"
+import { splitMarkdownPreamble } from "@/lib/utils"
 import MdxDocument from "./mdx/MdxDocument"
 
 type ChapterProps = {
@@ -10,18 +12,24 @@ export default async function Chapter({ chapter }: ChapterProps) {
   const doc = await data.getPieceDocument(chapter)
   const images = await data.getPieceAttachmentList(chapter, FileType.image)
   const chapterImageMap = new Map(images.map((ref) => [ref.filename, ref]))
+
+  const Document = ({ doc }: { doc: FileBuffer }) => {
+    const { body } = splitMarkdownPreamble(doc.buffer.toString())
+    return (
+      <div className="mx-5 py-5">
+        <MdxDocument
+          text={body}
+          imageMap={chapterImageMap}
+          className="bg-card rounded relative"
+        />
+      </div>
+    )
+  }
+
+
   return (
     <div key={chapter.hash} className="bg-card rounded relative">
-      {/* <ChapterHeader chapter={chapter} /> */}
-      {doc && (
-        <div className="mx-5 py-5">
-          <MdxDocument
-            text={doc.buffer.toString()}
-            imageMap={chapterImageMap}
-            className="bg-card rounded relative"
-          />
-        </div>
-      )}
+      {doc && <Document doc={doc} />}
     </div>
   )
 }
