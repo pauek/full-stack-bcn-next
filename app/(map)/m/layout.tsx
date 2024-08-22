b4b514381580ecd18748a3ba6f795cb3aa73d48e
Pau Fernández <pau.fernandez@upc.edu>
2024-08-22 12:56:35 +0200

    Moved Map to "layout.tsx"

--- app/(map)/m/layout.tsx
+++ app/(map)/m/layout.tsx
@@ -1,5 +1,7 @@
 import DarkModeAwareRoot from "@/components/DarkModeAwareBody"
 import "../../globals.css"
+import Map from "@/components/Map"
+import data from "@/lib/data"
 
 export const metadata = {
   title: "Full-stack Web Technologies",
@@ -10,12 +12,16 @@ type Props = {
   children: React.ReactNode
 }
 export default async function RootLayout({ children }: Props) {
+  const mapPositions = await data.getMapPositionsExtended()
   return (
     <DarkModeAwareRoot lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
       </head>
-      <body>{children}</body>
+      <body id="page-box">
+        <Map mapPositions={mapPositions} />
+        {children}
+      </body>
     </DarkModeAwareRoot>
   )
 }
--- app/(map)/m/page.tsx
+++ app/(map)/m/page.tsx
@@ -1,11 +1,3 @@
-import Map from "@/components/Map"
-import data from "@/lib/data"
-
 export default async function Page() {
-  const mapPositions = await data.getMapPositionsExtended()
-  return (
-    <div id="page-box">
-      <Map mapPositions={mapPositions} />
-    </div>
-  )
+  return <main></main>
 }
