9894fb818aae53169e0289f60dad28ebc28bf441
Pau Fernández <pau.fernandez@upc.edu>
2024-08-25 12:28:13 +0200

    Better buttons in the Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,25 +9,33 @@ export default async function Header() {
   if (!course) {
     throw new Error(`Course not found: "${env.COURSE_ID}"`)
   }
+
+  const Floating = ({ children }: { children: React.ReactNode }) => (
+    <div className="flex flex-row items-center gap-3 bg-card rounded-full shadow-md px-3 h-9 pointer-events-auto hover:bg-secondary">
+      {children}
+    </div>
+  )
+
   return (
     <header
       className={
         "p-2 fixed top-0 left-0 right-0 flex flex-row items-center justify-between z-20 overflow-visible pointer-events-none"
       }
     >
-      <div className="flex flex-row items-center gap-3 bg-card rounded-full shadow-lg px-3 h-9 pointer-events-auto">
-        <Link href="/m">
-          <MapIcon className="w-7 h-7 text-gray-600" />
-        </Link>
-
+      <div className="flex flex-row gap-2">
         <Link href="/" className="font-bold overflow-ellipsis">
-          {course.name}
+          <Floating>{course.name}</Floating>
+        </Link>
+        <Link href="/m">
+          <Floating>
+            <MapIcon className="w-7 h-7 text-gray-600" />
+          </Floating>
         </Link>
       </div>
 
-      <div className="flex flex-row items-center gap-3 bg-card rounded-full shadow-lg px-3 h-9 min-w-16 pointer-events-auto">
+      <Floating>
         <DarkModeSwitch />
-      </div>
+      </Floating>
     </header>
   )
 }
